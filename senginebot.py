f28a04529aa0fc699aefa15e1f62709a4d3dfa89
senatorQn <mail@kuhnp.de>
2019-07-06 17:31:01 +0200

    refactoring

--- senginebot.py
+++ senginebot.py
@@ -16,15 +16,15 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 # command handlers
-def start(bot, update):
+def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi! \n Try /start or /help')
+    update.message.reply_text('Hi! \n Try /start, /help or /eisbach')
 
-def help(bot, update):
+def help(update, context):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+    update.message.reply_text('Try /eisbach to get the current temperatur of eisbach.')
 
-def error(bot, update, error):
+def error(update, context, error):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, error)
 
@@ -37,44 +37,78 @@ def getEisbachTemp():
     if response is not None:
         html = BeautifulSoup(response.text, 'html.parser')
         temp_raw = html.findAll('h1', {"class": "value-big", "id": "water"})
-
+        
         temp = float(temp_raw[0].contents[0].replace(',','.'))
 
         return temp
 
 # message handler
-def buildMorningBriefing():
+def buildEisbachBriefing():
 
     temp = -273.0
-    temp = getEisbachTemp()
+    try:
+        temp = getEisbachTemp()
 
-    kalt = 'nur die harten kommen in den Garten...'
-    ok = 'man sieht sich im englischen ... Badehose nicht vergessen!'
-    warm = 'Absolute Badepflicht! Badehose optional.'
+        kalt = 'nur die harten kommen in den Garten...'
+        ok = 'man sieht sich im englischen ... Badehose nicht vergessen!'
+        warm = 'Absolute Badepflicht! Badehose optional.'
 
-    if temp <= 12:
-        eisbachscore = kalt
-    elif temp < 17 and temp >=13:
-        eisbachscore = ok
-    elif temp > 17:
-        eisbachscore = warm
+        if temp <= 12:
+            eisbachscore = kalt
+        elif temp < 17 and temp >=13:
+            eisbachscore = ok
+        elif temp > 17:
+            eisbachscore = warm
 
-    text='Guten Morgen! \n\nDer Eisbach hat aktuell ' + str(temp) + '°C - ' + eisbachscore + '\n\nQuelle: https://www.eisbachwetter.de/'
-    return text
+        text='Der Eisbach hat aktuell ' + str(temp) + '°C - ' + eisbachscore + '\n\nQuelle: https://www.eisbachwetter.de/'
+        return text
 
-def sendMorningBriefing(bot, update):
+    except:
+        return "Aktuell ist die Eisbach-Temperatur nicht verfügbar. Da gibt's nur eines: Ausprobieren! \n\nQuelle: https://www.eisbachwetter.de/"
 
-    chat_id_self = ''
-    chat_id_group = ''
+def sendEisbachBriefing(update, context):
+    """Send a message when the command /eisbach is issued."""
+    update.message.reply_text(buildEisbachBriefing())
 
-    with open('chat_id_self.txt', 'r') as t:
-        chat_id_self = str.strip(t.read())
+def sendMorningBriefing(context):
+    job = context.job
+    message ='Guten Morgen \n\n' + buildEisbachBriefing()
+    context.bot.send_message(job.context, text=message)
 
-    with open('chat_id_group.txt', 'r') as t:
-        chat_id_group = str.strip(t.read())
 
-    # bot.send_message(chat_id=chat_id_self, text=buildMorningBriefing()) # dev
-    bot.send_message(chat_id=chat_id_group, text=buildMorningBriefing()) # prod
+def setMorningBriefing(update, context):
+    """Add a job to the queue."""
+    chat_id = update.message.chat_id
+    try:
+        # args[0] should contain the time for the timer in seconds
+        # due = int(context.args[0])
+        # if due < 0:
+        #     update.message.reply_text('Sorry we can not go back to future!')
+        #     return
+
+        # Add job to queue
+        job = context.job_queue.run_daily(sendMorningBriefing, time = datetime.time(7, 00)) # send daily on time (for prod)
+        # job = context.job_queue.run_repeating(sendMorningBriefing, interval=30, first=0, context=chat_id) # send repeating (for dev)
+
+        context.chat_data['job'] = job
+
+        update.message.reply_text('Morning briefing successfully subscribed!')
+
+    except (IndexError, ValueError):
+        update.message.reply_text('Error')
+
+
+def unsetMorningBriefing(update, context):
+    """Remove the job if the user changed their mind."""
+    if 'job' not in context.chat_data:
+        update.message.reply_text('You have no active briefing')
+        return
+
+    job = context.chat_data['job']
+    job.schedule_removal()
+    del context.chat_data['job']
+
+    update.message.reply_text('Briefing successfully canceled!')
 
 # def ipp(bot, update):
 #     """Send IPP meals of the day when the command /ipp is issued."""
@@ -95,18 +129,19 @@ def main():
     token = ''
     with open('token.txt', 'r') as t:
         token = str.strip(t.read())
-    updater = Updater(token)
+    updater = Updater(token, use_context=True)
     dp = updater.dispatcher
 
     ### handle commands
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("eisbach", sendEisbachBriefing))
     # dp.add_handler(CommandHandler("ipp", ipp))
 
     ### timer
-    job = updater.job_queue.run_daily(sendMorningBriefing, time = datetime.time(7, 00)) # send daily on time (for prod)
-    # job = updater.job_queue.run_repeating(sendMorningBriefing, interval=30, first=0) # send repeating (for dev)
-
+    dp.add_handler(CommandHandler("getbriefing", setMorningBriefing, pass_args=True, pass_job_queue=True, pass_chat_data=True))
+    dp.add_handler(CommandHandler("stopbriefing", unsetMorningBriefing))
+    
     # log all errors
     dp.add_error_handler(error)
 
